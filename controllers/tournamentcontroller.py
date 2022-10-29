b07e3582176a5921a44a0be9d5dff0c6e07d5af2
ptitpouet <thierry.moncorger@agence3pp.com>
2022-10-29 21:36:40 +0200

    Menu Tournament (avec Tuple)

--- controllers/tournamentcontroller.py
+++ controllers/tournamentcontroller.py
@@ -1,12 +1,15 @@
+from views.homeview import HomeView
+
+
 class TournamentController:
 
-    def __init__(self, view):
+    def __init__(self, view, database):
         # views
         self.view = view
+        self.database = database
 
     def run(self):
         self.view.show_welcome()
-
         option = self.view.prompt_for_section()
 
         if option == 1:
@@ -14,6 +17,10 @@ class TournamentController:
         elif option == 2:
             pass
         elif option == 3:
-            pass
+            self.display_menu()
         else:
             pass
+
+    def back_home(self):
+        self.view = HomeView()
+        self.run()
--- views/tournamentmenuview.py
+++ views/tournamentmenuview.py
@@ -1,37 +1,30 @@
+OPTIONS = (
+    'Launch a Tournament',
+    'Reload a Tournament',
+    '< Exit >'
+)
+
+
 class TournamentView:
 
     def show_welcome(self):
         """Welcome the user"""
         print('~~ Tournament Management ~~')
-        print('°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°')
 
     def prompt_for_section(self):
-        """Prompt for the user's choice of what to do"""
-        menu_options = {
-            1: 'Launch a new Tournament',
-            2: 'Load a Tournament',
-            3: '- back'
-        }
+        """Prompt for the Main Menu"""
+        user_choice = 0
+        print("------------------------------------------------")
+        print("     > What do you want to do?")
 
         def print_menu():
-            for key in menu_options.keys():
-                print(key, '--', menu_options[key])
+            for value in OPTIONS:
+                print(OPTIONS.index(value) + 1, '--', value)
 
-        def print_option(option):
-            print('--> '+ str(option))
-
-        print("What do you want to do?")
         print_menu()
         try:
-            option = int(input('Enter your choice: '))
+            user_choice = int(input('Enter your choice: '))
         except:
-            print('Wrong input. Please enter a number ...')
-        if option >= 1 or option <= 2:
-            print_option(menu_options[option])
-        else:
-            print('Invalid option. Please enter a number between 1 and 4.')
+            print('!!! Wrong input. Please enter a number between 1 and ' + str(len(OPTIONS) + 1))
 
-        return option
-
-
-
+        return user_choice
